20ff0942da9dffd474854c2a4c485c998fcf4691
arai-wa <wataru.arai@jrits.ricoh.co.jp>
2016-04-14 17:48:55 +0900

    initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/**
+ * This code enables you to input tab(\t) into memos.
+ */
+	
+document.onkeydown = function (e) {
+    if (e.keyCode === 9) {
+        e.preventDefault();
+        event.returnValue = false;
+        document.execCommand('insertHTML',false , '\t');
+    }
+};
