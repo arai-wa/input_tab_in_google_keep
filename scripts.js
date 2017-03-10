78f0fd054baf3c4f255b6f697e5871bb991402ef
arai-wa <wataru.arai@jrits.ricoh.co.jp>
2017-03-10 15:46:22 +0900

    delete unused lines

--- scripts.js
+++ scripts.js
@@ -2,10 +2,9 @@
  * This code enables you to input tab(\t) into memos.
  */
 	
-document.onkeydown = function (e) {
-    if (e.keyCode === 9) {
-        e.preventDefault();
-        event.returnValue = false;
+document.onkeydown = (event) => {
+    if (event.keyCode === 9) {
+        event.preventDefault();
         document.execCommand('insertText', false, '\t');
     }
 };
